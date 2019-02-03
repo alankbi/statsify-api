4d8ba711d233db0dd3752f290d9df2495e68f529
Alan-Bi <alan.bi326@gmail.com>
2019-02-02 22:01:12 -0800

    Implement traversal through all subpages in Website

--- visualizer/website.py
+++ visualizer/website.py
@@ -1,10 +1,35 @@
+from visualizer.page import Page
+
+
 class Website:
     def __init__(self, root_page):
         self.root_page = root_page
         if root_page is not None:
-            self.pages = None   # { url: (Page, frequency) }
-            self.text = None    # Large string
-            self.key_phrases = None
-            self.word_count = None
+            self.pages = {root_page.url: (root_page, 1)}
+            self.text = root_page.text
+            self.word_count = 0
 
-            self.outbound_links = None
+            self.outbound_links = list(root_page.outbound_links)
+
+            self.traverse_all_pages()
+
+
+            self.key_phrases = None
+
+    def traverse_all_pages(self):
+        remaining_pages = [self.root_page]
+
+        while remaining_pages:
+            page = remaining_pages.pop()
+
+            if page.subpages is not None:
+                for link in page.subpages:
+                    if link in self.pages:
+                        self.pages[link] = (self.pages[link][0], self.pages[link][1] + page.subpages[link][1])
+                    else:
+                        self.pages[link] = (page.subpages[link][0], page.subpages[link][1])
+                        remaining_pages.append(page.subpages[link][0])
+
+
+website = Website(Page('http://alanbi.com', generate_subpages=True))
+print(website.pages)
